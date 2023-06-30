33885f45173ff0af89da9a40bc6f86bf007331c3
Scott Kennedy <scottyzen@gmail.com>
2023-06-30 12:14:30 +0100

    Bump version

--- woonuxt.php
+++ woonuxt.php
@@ -5,7 +5,7 @@ Description: This is a WordPress plugin that allows you to use the WooNuxt theme
 Author: Scott Kennedy
 Author URI: http://scottyzen.com
 Plugin URI: https://github.com/scottyzen/woonuxt-settings
-Version: 1.0.38
+Version: 1.0.40
 Text Domain: woonuxt
 GitHub Plugin URI: scottyzen/woonuxt-settings
 GitHub Plugin URI: https://github.com/scottyzen/woonuxt-settings
@@ -14,7 +14,7 @@ GitHub Plugin URI: https://github.com/scottyzen/woonuxt-settings
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-define( 'WOONUXT_SETTINGS_VERSION', '1.0.38' );
+define( 'WOONUXT_SETTINGS_VERSION', '1.0.40' );
 define( 'WPGraphQL_version', '1.13.8' );
 define( 'WooGraphQL_version', '0.13.0' );
 define( 'WPGraphQL_CORS_version', '2.1' );
