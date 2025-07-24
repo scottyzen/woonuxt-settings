7f2d6778c496729986214cfa8f72c45cf411f1e8
Scott Kennedy <scottyzen@gmail.com>
2025-07-24 23:11:35 +0100

    Bump version to 2.2.1 and update changelog for WP GraphQL Headless Login

--- includes/constants.php
+++ includes/constants.php
@@ -1,9 +1,9 @@
 <?php
 defined('ABSPATH') || exit;
 
-define('WOONUXT_SETTINGS_VERSION', '2.2.0');
+define('WOONUXT_SETTINGS_VERSION', '2.2.1');
 define('MY_WOOCOMMERCE_VERSION', '9.9.5');
 define('WP_GRAPHQL_VERSION', '2.3.3');
 define('WOO_GRAPHQL_VERSION', '0.21.2');
-define('WP_GRAPHQL_HEADLESS_LOGIN_VERSION', '0.4.2');
+define('WP_GRAPHQL_HEADLESS_LOGIN_VERSION', '0.4.3');
 define('WOONUXT_PLUGIN_FILE', __FILE__);
