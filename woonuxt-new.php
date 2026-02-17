3613d4ec41d452b3d9c576f236ecbb26808601ea
Scott Kennedy <scottyzen@gmail.com>
2026-02-17 20:39:45 +0000

    Fix plugin version

--- woonuxt-new.php
+++ woonuxt-new.php
@@ -3,14 +3,14 @@
  * Plugin Name: WooNuxt Settings
  * Plugin URI: https://github.com/scottyzen/woonuxt-settings
  * Description: WordPress plugin that allows you to use the WooNuxt theme with your WordPress site.
- * Version: 2.3.0
+ * Version: 2.5.0
  * Author: Scott Kennedy
  * Author URI: http://scottyzen.com
  * Text Domain: woonuxt
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * GitHub Plugin URI: scottyzen/woonuxt-settings
- * 
+ *
  * @package WooNuxt Settings
  * @since 1.0.0
  */
@@ -29,7 +29,7 @@ require_once WOONUXT_PLUGIN_DIR . 'includes/class-woonuxt-plugin.php';
 
 /**
  * Initialize the plugin
- * 
+ *
  * @since 2.3.0
  * @return WooNuxt_Plugin
  */
