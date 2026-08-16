98c6fe4aa0de09d45694bb2f1a4a929e8a6eebe6
Scott Kennedy <scottyzen@gmail.com>
2026-08-17 00:54:45 +0100

    Use major WordPress tested version

--- woonuxt.php
+++ woonuxt.php
@@ -7,7 +7,7 @@ Author URI: https://scottyzen.com
 Plugin URI: https://github.com/scottyzen/woonuxt-settings
 Version: 2.5.18
 Requires at least: 6.0
-Tested up to: 7.0.0
+Tested up to: 7.0
 Requires PHP: 8.4
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
